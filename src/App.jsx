013a78c2d15f63aac8024f482efa38bb0b4a54b5
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-10-20 23:54:40 +0000

    creando tablas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from 'react'
+
 import TaskCreator  from './components/TaskCreator.jsx'
 import './App.css'
+import {useState} from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [tasksItems, setTasksItems] = useState([
+    {
+      id: 1,
+      name: 'Task 1',
+      done: false
+    },
+    {
+      id: 2,
+      name: 'Task 2',
+      done: true
+    },
+    {
+      id: 3,
+      name: 'Task 3',
+      done: false
+    }
+  ])
+
+
 
   return (
     <>
       <TaskCreator/>
+      <table>
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasksItems.map((task) => (
+            <tr key={task.id}>
+              <td>{task.name}</td>
+              <td>{task.done ? 'Yes' : 'No'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
